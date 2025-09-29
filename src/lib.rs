61753f984f6de7d2be6611a64ce37d35708f23cf
gabrieldemarmiesse <gabriel@kyutai.org>
2025-09-29 12:18:26 +0000

    Add rolling repetitions to the documents

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,13 @@ use pyo3::prelude::*;
 mod bloom;
 mod code;
 mod entry;
+mod repetitions;
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn dactory(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(entry::dedup_document, m)?)?;
+    m.add_function(wrap_pyfunction!(repetitions::compute_repetitions_rolling, m)?)?;
     m.add_class::<bloom::BloomFilter>()?;
     m.add_class::<entry::FastTextPyWrapper>()?;
     Ok(())
